1549ca34dce65726ab7de260345e0b9d3d697aa0
JheDev <jhejentkmc@gmail.com>
2024-11-02 05:29:49 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,31 +1,24 @@
 var data = {
   name : [
-    {name: 'Name', value: 'Ahsan Khan'}
+    {name: 'Name', value: 'Jhe ID'}
   ],
   bio: [
-    {name: 'Name', value: 'Ahsan Khan'},
-    {name: 'Email', value: 'm.ahsankhan26@hotmail.com'},
-    {name: 'Phone', value: '+92-344-0200963'},
-    {name: 'Country', value: 'Pakistan'},
-    {name: 'City', value: 'Karachi'}
+    {name: 'Name', value: 'Jhe ID'},
+    {name: 'Email', value: 'jhedev@outlook.com'},
+    {name: 'Phone', value: '+62-856-9331xxxx'},
+    {name: 'Country', value: 'Indonesia'},
+    {name: 'City', value: 'Karawang'}
   ],
   github: [
-    {name: 'GitHub', value: 'ahsankhan26'}
+    {name: 'GitHub', value: 'jhedev96'}
   ],
   projects: [
-    {name: 'Book Blog', value: 'https://github.com/ahsankhan26/Book-Blog'},
-    {name: 'Bomberman Replica', value: 'https://github.com/ahsankhan26/Bomberman-Replica'},
-    {name: 'WhatsApp Chat Parser', value: 'https://github.com/ahsankhan26/Whatsapp-Chat-Parser'},
-    {name: 'Laser Data Transmission', value: 'https://github.com/ahsankhan26/Laser-Data-Transmission'},
-    {name: 'Color Guessing Game', value: 'https://github.com/ahsankhan26/Color-Guessing-Game'},
-    {name: 'GPA Calculator', value: 'https://github.com/ahsankhan26/gpa-calculator'},
-    {name: 'Hello World but unnecessarily complex', value: 'https://github.com/ahsankhan26/hello-world'}
+    {name: 'Terminal Portfolio', value: 'https://github.com/jhedev96/terminal-portfolio'}
   ],
-  resume: 'Ahsan_Khan_CV.pdf',
+  resume: 'j.pdf',
   socials: [
-    {name: 'LinkedIn', value: 'https://linkedin/in/ahsankhan26'},
-    {name: 'GitHub', value: 'https://github.com/ahsankhan26'},
-    {name: 'Instagram', value: 'https://instagram/ahsankhan26'},
-    {name: 'Website', value: 'https://ahsankhan.me'}
+    {name: 'GitHub', value: 'https://github.com/jhedev96'},
+    {name: 'Instagram', value: 'https://instagram/jhedev96'},
+    {name: 'Website', value: 'https://jhedev.my.id'}
   ]
 }
